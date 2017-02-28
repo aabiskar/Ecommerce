347afeed0a3ee42363cc3340ebbc20509c0b7416
Manoj <manojkhatri2069@gmail.com>
2017-02-28 13:00:49 +0545

    connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express');
-
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var config = require('./config');
 var app = express();
+
+mongoose.connect(config.database, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to the database");
+    }
+});
+
+
+//Middleware
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    var name = "Manoj";
+    res.json("My name is " + name);
+});
 
 app.listen(3000, function(err) {
     if (err) throw err;
