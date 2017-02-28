5957a2b95b42203ba9d9096fe81d847719979a34
Manoj <manojkhatri2069@gmail.com>
2017-02-28 13:02:43 +0545

    config for database

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "database": "mongodb://localhost/ecommerce", //database: the URI with username and password to your MongoDB installation
+    //"port": process.env.PORT || 3000,
+    //"secretKey": "YourSecretKey"   //secret: used when we create and verify JSON Web Tokens
+}
