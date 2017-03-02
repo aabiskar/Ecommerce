5a2965f06a6d969f216a99d5cb0c3677cf74c2d8
Manoj <manojkhatri2069@gmail.com>
2017-03-02 17:11:21 +0545

    signup3Completed

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ var router = require('express').Router();
 var User = require('../models/user');
 
 router.get('/signup', function(req, res, next) {
-    res.render('accounts/signup');
+    res.render('accounts/signup', {
+        errors: req.flash('errors')
+    });
 });
 
 router.post('/signup', function(req, res, next) {
@@ -11,14 +13,14 @@ router.post('/signup', function(req, res, next) {
     user.email = req.body.email;
     user.password = req.body.password;
 
-    User.fineOne({ email: req.body.email }, function(err, existingUser) {
+    User.findOne({ email: req.body.email }, function(err, existingUser) {
         if (existingUser) {
-            console.log(req.body.email + "is already exist");
+            req.flash('errors', 'Account with that email address already exists');
+            return res.redirect('/signup');
         } else {
             user.save(function(err, user) {
                 if (err) return next(err);
-
-                res.json("New user has been created");
+                return res.redirect('/');
             });
         }
     });
--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var config = require('./config');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+
+
 var User = require('./models/user');
 
 var app = express();
@@ -25,6 +30,15 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "Manoj@!@"
+}));
+app.use(flash());
+
+
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
