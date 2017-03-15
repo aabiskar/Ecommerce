a08574b54be1b7954c50b8c219dff65f19419d5d
Manoj <manojkhatri2069@gmail.com>
2017-03-15 13:57:07 +0545

    Created GET cart

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var Category = require('../models/category');
+var Cart = require('../models/cart');
 var Product = require('../models/product');
 
 function paginate(req, res, next) {
@@ -49,6 +50,37 @@ stream.on('close', function() {
 stream.on('error', function(err) {
     console.log(err);
 });
+
+router.get('/cart', function(req, res, next) {
+    Cart
+        .findOne({ owner: req.user._id })
+        .populate('items.item')
+        .exec(function(err, foundCart) {
+            if (err) return next(err);
+            res.render('main/cart', {
+                foundCart: foundCart,
+                message: req.flash('remove')
+            });
+        });
+});
+
+router.post('/product/:product_id', function(req, res, next) {
+    Cart.findOne({ owner: req.user._id }, function(err, cart) {
+        cart.items.push({
+            item: req.body.product_id,
+            price: parseFloat(req.body.priceValue),
+            quantity: parseInt(req.body.quantity)
+        });
+
+        cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2);
+
+        cart.save(function(err) {
+            if (err) return next(err);
+            return res.redirect('/cart');
+        });
+    });
+});
+
 
 router.post('/search', function(req, res, next) {
     res.redirect('/search?q=' + req.body.q);
