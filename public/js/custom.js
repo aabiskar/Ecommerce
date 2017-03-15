1e175b06cd127208f4e7f69fe43ce8fa9aa2580d
Manoj <manojkhatri2069@gmail.com>
2017-03-15 13:57:35 +0545

    Added + & - button events

--- public/js/custom.js
+++ public/js/custom.js
@@ -40,6 +40,36 @@ $(function() {
             }
         });
     });
+});
+
+$(document).on('click', '#plus', function(e) {
+    e.preventDefault();
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+
+    priceValue += parseFloat($('#priceHidden').val());
+    quantity += 1;
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+});
+
+$(document).on('click', '#minus', function(e) {
+    e.preventDefault(); // Prevents reloading the page
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
 
 
-})
+    if (quantity == 1) { // Its prevents quantity to be less than 1
+        priceValue = $('#priceHidden').val();
+        quantity = 1;
+    } else {
+        priceValue -= parseFloat($('#priceHidden').val());
+        quantity -= 1;
+    }
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+});
