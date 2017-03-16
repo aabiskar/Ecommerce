710aaf00f2c42a6925fc782ed83e957eb805c445
Manoj <manojkhatri2069@gmail.com>
2017-03-16 15:43:29 +0545

    Added spinner

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,6 +3,48 @@ $(function() {
     // This identifies your website in the createToken call below
     Stripe.setPublishableKey('pk_test_Lx0mT8giZKdlBjWWtATV1vJF');
 
+    var opts = {
+        lines: 13 // The number of lines to draw
+            ,
+        length: 28 // The length of each line
+            ,
+        width: 14 // The line thickness
+            ,
+        radius: 42 // The radius of the inner circle
+            ,
+        scale: 1 // Scales overall size of the spinner
+            ,
+        corners: 1 // Corner roundness (0..1)
+            ,
+        color: '#000' // #rgb or #rrggbb or array of colors
+            ,
+        opacity: 0.25 // Opacity of the lines
+            ,
+        rotate: 0 // The rotation offset
+            ,
+        direction: 1 // 1: clockwise, -1: counterclockwise
+            ,
+        speed: 1 // Rounds per second
+            ,
+        trail: 60 // Afterglow percentage
+            ,
+        fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            ,
+        zIndex: 2e9 // The z-index (defaults to 2000000000)
+            ,
+        className: 'spinner' // The CSS class to assign to the spinner
+            ,
+        top: '50%' // Top position relative to parent
+            ,
+        left: '50%' // Left position relative to parent
+            ,
+        shadow: false // Whether to render a shadow
+            ,
+        hwaccel: false // Whether to use hardware acceleration
+            ,
+        position: 'absolute' // Element positioning
+    }
+
     $('#search').keyup(function() {
 
         var search_term = $(this).val();
@@ -90,6 +132,10 @@ $(function() {
             // Insert the token into the form so it gets submitted to the server
             $form.append($('<input type="hidden" name="stripeToken" />').val(token));
             // and submit
+
+            var spinner = new Spinner(opts).spin();
+            $('#loading').append(spinner.el);
+
             $form.get(0).submit();
         }
     };
