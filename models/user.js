07d34a18fb928ae9cf717270186b27516810035e
Manoj <manojkhatri2069@gmail.com>
2017-03-16 15:43:11 +0545

    Added item on the history property of UserSchema

--- models/user.js
+++ models/user.js
@@ -15,13 +15,9 @@ var UserSchema = new Schema({
 
     address: String,
     history: [{
-        date: Date,
         paid: { type: Number, default: 0 },
-
+        item: { type: Schema.Types.ObjectId, ref: 'Product' }
     }]
-
-
-
 });
 
 
